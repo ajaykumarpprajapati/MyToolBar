0239caa66cf64acef7654bc76c228f34a6fb0e1b
ajay <ajay.prajapati@neoquant.com>
2019-05-28 12:51:28 +0530

    commit

--- app/src/main/java/com/neoqunat/mytoolbarapp/MainActivity.kt
+++ app/src/main/java/com/neoqunat/mytoolbarapp/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.neoqunat.mytoolbarapp
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import com.neoqunat.utils.ToastUtils
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private var toolbar: Toolbar? = null
+    private var drawerLayout: DrawerLayout? = null
+    private var navigationView: NavigationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +23,29 @@ class MainActivity : AppCompatActivity() {
         toolbar = findViewById(R.id.tool_bar)
         setSupportActionBar(toolbar)
         toolbar?.title = "ToolBar"
+        //show back button to toolbar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
 
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.navigation_view)
+        navigationView?.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar,
+            R.string.open_drawer, R.string.close_drawer)
+
+        drawerLayout?.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onBackPressed() {
+        drawerLayout?.let {
+            if(it.isDrawerOpen(GravityCompat.START)){
+                it.closeDrawer(GravityCompat.START)
+            } else {
+                super.onBackPressed()
+            }
+        }
     }
 
     // region Override Method of Menu
@@ -51,5 +78,33 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when(menuItem.itemId){
+            R.id.inbox_menu -> {
+                ToastUtils.showToast(this, getString(R.string.inbox))
+            }
+            R.id.starred_menu -> {
+                ToastUtils.showToast(this, getString(R.string.starred))
+            }
+            R.id.sent_menu -> {
+                ToastUtils.showToast(this, getString(R.string.sent))
+            }
+            R.id.draft_menu -> {
+                ToastUtils.showToast(this, getString(R.string.draft))
+            }
+            R.id.all_mail_menu -> {
+                ToastUtils.showToast(this, getString(R.string.all_mail))
+            }
+            R.id.trash_menu -> {
+                ToastUtils.showToast(this, getString(R.string.thrash))
+            }
+            R.id.spam_menu -> {
+                ToastUtils.showToast(this, getString(R.string.spam))
+            }
+
+        }
+        drawerLayout?.closeDrawers()
+        return true
+    }
     // endregion
 }
---
+++ app/src/main/java/com/neoqunat/utils/ToastUtils.kt
@@ -0,0 +1,12 @@
+package com.neoqunat.utils
+
+import android.content.Context
+import android.widget.Toast
+
+object ToastUtils{
+
+    fun showToast(context: Context, message: String){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+}
